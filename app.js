b4c33aa43f3363d3ec50c0598656c666976d4d06
salmansrizon <salmansrizon2016@gmail.com>
2024-01-31 19:31:29 +0600

    test

---
+++ app.js
@@ -0,0 +1,12 @@
+const amount = 12
+
+if (amount < 10){
+    console.log('Small Number')
+} else{
+    console.log("large number")
+}
+
+const { log } = require('console')
+const path = require('path')
+
+console.log
