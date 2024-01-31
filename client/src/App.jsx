981b1ea45c2456e44f8003088748c7efbecae85a
salmansrizon <salmansrizon2016@gmail.com>
2024-01-31 19:59:00 +0600

    Route added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/about'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Project from './pages/project'
+import dashboard from './pages/dashboard'
 
 export default function App() {
   return (
-    <h1 className='text-3xl text-red-500'>
-      This is a test
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path='/about' element={<About />} />
+        <Route path='/signin' element={<Signin />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/project' element={<Project />} />
+        <Route path='/dashboard' element={<dashboard />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
