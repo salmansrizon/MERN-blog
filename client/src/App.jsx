a93a3d964b79237f9d5f002196bd451d432f7059
salmansrizon <salmansrizon2016@gmail.com>
2024-01-31 21:43:41 +0600

    headed added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import Signin from './pages/Signin'
 import Signup from './pages/Signup'
 import Project from './pages/project'
 import dashboard from './pages/dashboard'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path='/about' element={<About />} />
