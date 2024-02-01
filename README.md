d3197873de38c74d659724c1d996f25e5a1aafd4
salmansrizon <salmansrizon2016@gmail.com>
2024-02-01 08:43:43 +0600

    Connetion to databse

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# MERN-blog
+# Blogging website development with MERN
+
+Main goal of this project is to showcase and demonstarte the learnings of `MERN` development lifecysle and steps.
+
+| Packege name     |  Purpose| Used Type          |
+| ------------- |:-------------:|:-------------:| 
+| `Vite`      | VIte is a build tool that aims to provide a faster and leaner development experience for modern web projects | Frontend Tooling |
+| col 2 is      | centered      |   $12 |
+| zebra stripes | are neat      |    $1 |
--- api/index.js
+++ api/index.js
@@ -1,7 +1,23 @@
-import exprss from "express";
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
 
-const app = exprss();
+// initiating credential from enviornment veriable 
+dotenv.config();
+
+// Connection to database
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDb is connected");
+  })
+  .catch((err) => {
+    console.log(err);  //database auth error
+  });
+
+// initiate express
+const app = express();
 
 app.listen(3000, () => {
-  console.log("Server is running at 2000!");
+  console.log("Server is running at 3000!");
 });
