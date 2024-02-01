b58b154ee3961b5e38fe60edefa74b93f286bf29
salmansrizon <salmansrizon2016@gmail.com>
2024-02-01 09:09:59 +0600

    Create User module

---
+++ api/models/user.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamp: true } //to store user input timeline
+);
+
+// Initiating Model
+const User = mongoose.model("User", userSchema);
+
+// Exporting the Schema
+export default User;
