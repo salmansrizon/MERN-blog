24834e4700bbf0736514ceda8ea7c791454bab43
salmansrizon <salmansrizon2016@gmail.com>
2024-02-07 21:18:19 +0600

    Redux added in signin page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Redux
+import { store } from './redux/store.js'
+import { Provider } from 'react-redux'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>
 )
