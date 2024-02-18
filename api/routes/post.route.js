68432fd90f8e43b34b5557845b818436c0143af5
salmansrizon <salmansrizon2016@gmail.com>
2024-02-18 08:50:02 +0600

    Create get post api route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { varifyToken } from "../utils/varifyUser.js";
-import { create } from "../controllers/post.controller.js";
+import { create, getposts } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
 router.post("/create", varifyToken, create)
+router.get("/getposts", getposts)
 
 export default router;
