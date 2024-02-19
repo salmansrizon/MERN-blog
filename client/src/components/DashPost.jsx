4b10d6a688d108bce2c9758130a86d3ab24b4bfe
salmansrizon <salmansrizon2016@gmail.com>
2024-02-19 15:31:12 +0600

    show user post inside dashboard

--- client/src/components/DashPost.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function DashPost() {
-  return (
-    <div>
-      Post
-    </div>
-  )
-}
--- client/src/pages/dashboard.jsx
+++ client/src/pages/dashboard.jsx
@@ -2,7 +2,7 @@ import { useEffect, userEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import DashSidebar from "../components/DashSidebar";
 import DashProfile from "../components/DashProfile";
-import DashPost from "../components/DashPost";
+import DashPost from "../components/DashPosts";
 
 export default function Dashboard() {
   const location = useLocation();
